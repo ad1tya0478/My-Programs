f41546df8909c84a77d339dd36143e2d50dd3ca7
ad1tya0478 <aditya.50478@gmail.com>
2025-07-01 16:53:00 +0530

    Basics of java

--- Java/Variables.java
+++ Java/Variables.java
@@ -22,8 +22,51 @@ public class Variables {
 
 
         // Type Conversion and casting 
-        float f = 5.6f;
-        int x = (int)f;
-        System.out.println(x);
+
+        // float f = 5.6f;
+        // int x = (int)f;
+        // System.out.println(x);
+
+        // byte b = 127;
+        // int n = b;
+        // System.out.println(n);
+
+        // int h = 257;
+        // byte k = (byte) h;
+        // System.out.println(k);
+
+        // byte b = 10;
+        // byte c = 30;
+        // int result = b * c;
+        // System.out.println(result);
+
+        ///// ARITHMETIC OPERATORS ///
+        
+        // int num1 = 7;
+        // // int num2 = 5;
+        // // int result = num1 * num2;
+
+        // num1 += 2;
+        // System.out.println(num1);
+
+
+        ////// Relational operators 
+        
+        // int x = 9;
+        // int y = 6;
+        // boolean result = x == y;
+        // boolean result = x >= y;
+        // boolean result = x != y;
+        // System.out.println(result);
+
+        ///// Logical operators 
+        int x = 7;
+        int y = 5;
+        int n = 5;
+        int m = 9;
+
+        boolean result = x>y || n > m ;
+        System.out.println(result);
+
     }
 }
