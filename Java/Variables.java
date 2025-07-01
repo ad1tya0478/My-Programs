aba2cad9710bf1135a9136da4e2761d0c7c4192c
ad1tya0478 <aditya.50478@gmail.com>
2025-07-01 15:53:10 +0530

    Started java today

---
+++ Java/Variables.java
@@ -0,0 +1,29 @@
+public class Variables {
+    public static void main(String a[]) {
+        // int num1 = 9;
+        // byte by = 127;
+        // short sh = 558;
+        // long l = 5848l;
+
+        // float f = 6.7f;
+        // double d = 6.8;
+
+        // char c = 'k';
+
+        // boolean b = true;
+
+        // literals 
+        // int num1 = 0b101;
+        // System.out.println(num1);
+
+        // char c = 'a';
+        // c++;
+        // System.out.println(c);
+
+
+        // Type Conversion and casting 
+        float f = 5.6f;
+        int x = (int)f;
+        System.out.println(x);
+    }
+}
---
+++ Java/firstprogram.java
@@ -0,0 +1,14 @@
+
+public class firstprogram {
+    public static void main(String a[]) {
+        int num = 9;
+        int num2 = 9;
+        int result = num + num2;
+        System.out.println(result);
+        
+        
+        float marks = 9.5f;
+        System.out.println(marks); 
+    }
+
+}
