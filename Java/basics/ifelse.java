9d742dcb1fe85e727e1bbb8e6bbf8a3b600427eb
ad1tya0478 <aditya.50478@gmail.com>
2025-07-02 16:23:51 +0530

    If-else Java

---
+++ Java/basics/ifelse.java
@@ -0,0 +1,19 @@
+public class ifelse {
+    public static void main (String a[]){
+        
+        // int x = 98;
+        // if (x>10 && x<=20)
+        // System.out.println("Hello");
+        // else 
+        // System.out.println("Bye");
+
+        int x = 8;
+        int y = 7;
+        if (x>y) {
+            System.out.println(x);
+            System.out.println("Thank you");
+        } else {
+            System.out.println(y);
+        }
+    }
+}
