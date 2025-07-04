6c6fda2fe7bf338f3bda8caf91afc8b971ca6c61
ad1tya0478 <aditya.50478@gmail.com>
2025-07-04 11:22:46 +0530

    Array and its Ques in Java

---
+++ Java/basics/array/array.java
@@ -0,0 +1,40 @@
+public class array {
+    public static void main(String a[]){
+        // int num[] = {1,2,3,4,5};
+        
+        // for(int i = 0;i<num.length;i++){
+        //     System.out.println(num[i]);
+        // }
+
+
+        // Q.1 
+        // int num[] = {1,2,3,56,6};
+        // int max = num[0];
+        // for(int i = 1;i<num.length;i++){
+        //     if(num[i] > max){
+        //         max = num[i];
+        //     }
+        // }
+        // System.out.println("Max element: " + max);
+
+
+        // Q.2 
+        // int num[] = {2,4,6,8};
+        // int counteven = 0;
+        // int countodd = 0;
+        // for(int i = 0; i<num.length;i++){
+        //     if(num[i]%2 == 0){
+        //         counteven++;
+        //     } else 
+        //      countodd++;
+        // } 
+        // System.out.println("Even count: " + counteven);
+        // System.out.println("Odd count: " + countodd);
+
+
+        // Q.3 
+        
+
+        
+    }
+}
