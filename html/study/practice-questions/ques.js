d969eb1e4634a06159efb50ba3446c80789a901d
ad1tya0478 <aditya.50478@gmail.com>
2025-07-05 20:38:15 +0530

    Leetcode ques

--- html/study/practice-questions/ques.js
+++ html/study/practice-questions/ques.js
@@ -420,3 +420,18 @@ var createCounter = function(init) {
     }
 };
 
+
+
+var findLucky = function(arr) {
+    const freq = {};
+    for (let num of arr) {
+        freq[num] = (freq[num] || 0) + 1;
+    }
+    let maxLucky = -1;
+    for (let num in freq) {
+        if (parseInt(num) === freq[num]) {
+            maxLucky = Math.max(maxLucky, parseInt(num));
+        }
+    }
+    return maxLucky;
+};
