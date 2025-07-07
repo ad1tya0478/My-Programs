89e71ab14f543a245826b6f95a8267750bc39828
ad1tya0478 <aditya.50478@gmail.com>
2025-07-07 20:00:53 +0530

    2d Array ques'

--- Java/basics/array/array.java
+++ Java/basics/array/array.java
@@ -115,8 +115,26 @@ public class array {
         //     System.out.println();
         // }
 
-        
+        // Q.2
+        // int num[][] = {{1,2,3,4}, {5,6,7,8}};
 
+        // System.out.println("Original Matrix: ");
+        // for(int i = 0; i < num.length;i++){
+        //     for(int j = 0; j< num[i].length;j++){
+        //         System.out.print(num[i][j] + " ");
+        //     }
+        //     System.out.println();
+        // }
+
+        // System.out.println("Transpose Matrix: ");
+        // for(int i = 0; i < num[0].length;i++){
+        //     for(int j = 0; j< num.length ;j++){
+        //         System.out.print(num[j][i] + " ");
+        //     }
+        //     System.out.println();
+        // }
+        
+        
         
     }
 }
