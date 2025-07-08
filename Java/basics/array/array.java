2544c1af43a9691d228fe129a9f22d45296070b4
ad1tya0478 <aditya.50478@gmail.com>
2025-07-08 22:31:06 +0530

    Array of objects

--- Java/basics/array/array.java
+++ Java/basics/array/array.java
@@ -1,5 +1,5 @@
-public class array {
-    public static void main(String a[]){
+// public class array {
+//     public static void main(String a[]){
         // int num[] = {1,2,3,4,5};
         
         // for(int i = 0;i<num.length;i++){
@@ -184,7 +184,56 @@ public class array {
         //     }
         //     System.out.println();
         // }
+
+//     }
+// }
+
+
+
+/////////// Array Of objects ////////
         
+        // int nums[] = new int[4];
+        // nums[0] = 4;
+        // nums[1] = 8;
+        // nums[2] = 3;
+        // nums[3] = 9;
+        
+        // for(int i = 0;i<nums.length;i++){
+            //     System.out.println(nums[i]);
+            // }
+            
+            // class Student {
+            //     int rollno;
+            //     String name;
+            //     int marks;
+            // }
+public class array {
+    public static void main(String a[]){
+
+        // Student s1 = new Student();
+        // s1.rollno = 1;
+        // s1.name = "navin";
+        // s1.marks = 67;
+
+        // Student s2 = new Student();
+        // s2.rollno = 3;
+        // s2.name = "harsh";
+        // s2.marks = 70;
+
+        // Student s3 = new Student();
+        // s3.rollno = 3;
+        // s3.name = "aditya";
+        // s3.marks = 87;
+
+        // Student students[] = new Student[3];
+        // students[0] = s1;
+        // students[1] = s2;
+        // students[2] = s3;
+
+        for(int i = 0;i<students.length;i++){
+            System.out.println(students[i].name + " : " + students[i].marks);
+        }
+
     }
 }
 
