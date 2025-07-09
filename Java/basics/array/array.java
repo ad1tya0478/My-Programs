3c2abeed75741f5d8b1be602730fe54ea8eeb04e
ad1tya0478 <aditya.50478@gmail.com>
2025-07-09 12:57:30 +0530

    Strings

--- Java/basics/array/array.java
+++ Java/basics/array/array.java
@@ -193,41 +193,41 @@
 /////////// Array Of objects ////////
         
 
-class Student {
-        int rollno;
-        String name;
-        int marks;
-            }
+// class Student {
+//         int rollno;
+//         String name;
+//         int marks;
+//             }
 public class array {
     public static void main(String a[]){
                     
-        Student s1 = new Student();
-        s1.rollno = 1;
-        s1.name = "navin";
-        s1.marks = 67;
+        // Student s1 = new Student();
+        // s1.rollno = 1;
+        // s1.name = "navin";
+        // s1.marks = 67;
         
-        Student s2 = new Student();
-        s2.rollno = 3;
-        s2.name = "harsh";
-        s2.marks = 70;
+        // Student s2 = new Student();
+        // s2.rollno = 3;
+        // s2.name = "harsh";
+        // s2.marks = 70;
         
-        Student s3 = new Student();
-        s3.rollno = 3;
-        s3.name = "aditya";
-        s3.marks = 87;
+        // Student s3 = new Student();
+        // s3.rollno = 3;
+        // s3.name = "aditya";
+        // s3.marks = 87;
         
-        Student students[] = new Student[3];
-        students[0] = s1;
-        students[1] = s2;
-        students[2] = s3;
+        // Student students[] = new Student[3];
+        // students[0] = s1;
+        // students[1] = s2;
+        // students[2] = s3;
         
         // for(int i = 0;i<students.length;i++){
         //     System.out.println(students[i].name + " : " + students[i].marks);
         // }
-        
-        for(Student s : students ){
-            System.out.println(s.name + " : " + s.marks);
-        }
+
+        // for(Student s : students ){
+        //     System.out.println(s.name + " : " + s.marks);
+        // }
 
 
         /// Enhanced For Loop ///
@@ -243,6 +243,23 @@ public class array {
         //     System.out.println(n);
         // }
 
+
+///////// Strings ////////
+
+        // String name = new String("Aditya");
+        String name = "Aditya";
+
+        // System.out.println(name);
+        // System.out.println("Hello " + name);
+        // System.out.println(name.hashCode());
+        // System.out.println(name.charAt(1));
+        // System.out.println(name.concat(" Sharma"));
+
+        name = name + " Sharma";
+        System.out.println("Hello " + name);
+
+        StringBuffer sb = new StringBuffer();
+        System.out.println(sb.capacity());
 
     }
 }
