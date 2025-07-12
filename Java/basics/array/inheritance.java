f264bbedb489e3bf84972d6e0ad7eef05e8d4a7d
ad1tya0478 <aditya.50478@gmail.com>
2025-07-12 22:48:25 +0530

    Multi-level Inheritance

--- Java/basics/array/inheritance.java
+++ Java/basics/array/inheritance.java
@@ -3,32 +3,48 @@
 // Extends - it lets one class inherit from another 
 // Multi-Level inheritance 
 // Single-level inheritance 
-// 
+// multiple inheritance we cannot use in java  
 
 
-class calc {
-    public int add(int n1, int n2){
-        return n1 + n2;
-    }
-    public int sub(int n1, int n2){
-        return n1 - n2;
-    }
-    public int multi(int n1, int n2){
-        return n1 * n2;
-    }
-    public int div(int n1, int n2){
-        return n1 / n2;
-    }
-}
+// class calc {
+//     public int add(int n1, int n2){
+//         return n1 + n2;
+//     }
+//     public int sub(int n1, int n2){
+//         return n1 - n2;
+//     }
+//     public int multi(int n1, int n2){
+//         return n1 * n2;
+//     }
+//     public int div(int n1, int n2){
+//         return n1 / n2;
+//     }
+// }
 
-public class inheritance {
-    public static void main(String []args){
-        calc obj = new calc();
-        int r1 = obj.add(10,10);
-        int r2 = obj.sub(10,10);
-        int r3 = obj.multi(10, 10);
-        int r4 = obj.div(10,10);
+// public class inheritance {
+//     public static void main(String []args){
+//         calc obj = new calc();
+//         int r1 = obj.add(10,10);
+//         int r2 = obj.sub(10,10);
+//         int r3 = obj.multi(10, 10);
+//         int r4 = obj.div(10,10);
 
-        System.out.println(r1 + " " + r2 + " " + r3 + " " + r4);
-    }
-}
+//         System.out.println(r1 + " " + r2 + " " + r3 + " " + r4);
+//     }
+// }
+
+
+// class A {
+
+// }
+// class B extends A{
+
+// }
+// class C extends B{
+
+// }
+// public class inheritance{
+//     public static void main(String []args){
+
+//     }
+// }
