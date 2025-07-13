1a7210e2456ca5c90d87128f6f24f84e9abd4a92
ad1tya0478 <aditya.50478@gmail.com>
2025-07-13 23:13:59 +0530

    Read write and removing elements/attributes

--- html/study/tapascript/DOM.js
+++ html/study/tapascript/DOM.js
@@ -2,22 +2,68 @@
 // page while it's running 
 
 
-// Creating elements 
+/*
+// Creating elements //
 const plm = document.createElement("p");   // Creating a new HTML Element, a <p>
 plm.innerText = "This is a text added dynamically";   // Setting the text content of the new <p> tag
 document.body.appendChild(plm);   // First pointing to the body and then adding <p>, appendChild = adding a new element at the end of the line, always 
 console.log(plm);
+*/
 
 
-
-// Insert Elements 
+/*
+// Insert Elements //
 const span = document.createElement("span");
 span.innerText = "I am a span";
 
 const plme = document.querySelector("p"); // Finds the p tag on othe page and stores it in plme.
 // console.log(plme.nextElementSibling);
-document.body.insertBefore(span, plme);  // Placing the span element before the plme element inside the body, so that it will appear above 
+// document.body.insertBefore(span, plme);  // Placing the span element before the plme element inside the body, so that it will appear above 
 // the plme element.
+document.body.insertBefore(span, plme.nextElementSibling);  // Inserting span before the next sibling of plme, putting after plme but before the next element, in the middle.
+*/
+
+/*
+// Modifying Content // 
+const plm = document.querySelector("p"); // Queryselector selects the first element that matches a css-style, in this we are selecting p tag 
+console.log(plm.innerHTML);  // innerhtml get or sets the HTML content inside an element. Show tags + text inside
+
+plm.innerHTML = "<u>Hello</u> How are you doing.?";  // do not set innerHTML with user input, you open the gates to XSS attacks,
+//  a hacker can put any malicious script in your code, to save yourself, use libraries like DOMPurify
+
+const divl = document.querySelector("div");
+
+console.log("Inner text: ", divl.innerText);  // innerText - doesn't show a text if it is hidden, slower
+console.log("Text Content: ", divl.textContent); // textContent - get all text, hidden or not, better for performance and raw text
+*/
+
+/*
+// Removing elements // 
+let list = document.getElementById("mylist");
+const remove = list.children[0];
+list.removeChild(remove);  // Removechild - removes a specific child from a parent 
+
+const plm = document.createElement("p");
+list.replaceChildren(plm);  // Replace Child - Erase all existng children, the adds new ones(if any).
+
+document.getElementById("removeMe").remove();  // Remove - Removes the elements itself, no parent needed.
+*/
+
+/*
+//  Read, Write and Remove Attributes // 
+const image = document.querySelector("img");
+
+console.log(image.getAttribute('alt'));
+
+image.setAttribute("src", "./wow.png");
+image.setAttribute("alt", "Nasdaq Stock Exchange");
+
+image.removeAttribute("height");
+
+image.hasAttribute("src");  // True
+image.hasAttribute("height"); // False
+*/
 
 
+// Traversing/Navgating DOM //
 
