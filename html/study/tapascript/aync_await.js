b0e3980820bf47562455b12c265c373f1946c9ae
ad1tya0478 <aditya.50478@gmail.com>
2025-07-16 21:03:29 +0530

    Error Handling

--- html/study/tapascript/aync_await.js
+++ html/study/tapascript/aync_await.js
@@ -4,12 +4,12 @@
 
 
 // Problems With the promise 
-const promise = new Promise(function(resolve, reject){
-    setTimeout(() => {
-        resolve("I am a promise");
-    }, 2000)
-});
-promise.then((x) => console.log(x)); 
+// const promise = new Promise(function(resolve, reject){
+//     setTimeout(() => {
+//         resolve("I am a promise");
+//     }, 2000)
+// });
+// promise.then((x) => console.log(x)); 
 
 // Syntax
 // if we use async key word before a function and then run it, it will always return a promise
@@ -17,9 +17,12 @@ promise.then((x) => console.log(x));
 
 // return Promise.resolve(101) - it will return Promise { <pending> }
 // return 101 - in this it will return the 101 wrapped in promise 
-async function fool(){
-    return Promise.resolve(101);
-}
+
+
+// async function fool(){
+//     return Promise.resolve(101);
+// }
+
 
 // instead of doing this //
 // fool().then(function(result) {
@@ -29,17 +32,43 @@ async function fool(){
 // you can do this - we can you use await because it will also print only the result that we were doing with .then, so intead of using 
 // .then() all the time you can use await to get the result. so to use it just use the await, in front of the async funation, to get the result(resolve)
 
+
 // const result = await fool();
 // console.log(result);
+
 
 // we cannot use await outside of a async function, so in the upper case you can put the await into another async function to execute 
 // in simple words you cannot use await function globally 
 // if we don't put it correctly, it will give a syntax error in the console
 
-async function tackle() {
-    const result = await fool();
-    console.log(result);
-}
-console.log(tackle());  // now it will give you a promise with pending and undefined value, because you are logging a promise not the result of it 
+// async function tackle() {
+//     const result = await fool();  // Now here the execution will be halted, until the promsie is settled
+//     console.log(result);
+// }
+// console.log(tackle());  // now it will give you a promise with pending and undefined value, because you are logging a promise not the result of it 
 //                         because tackle() is async, it reutnrs a promise not the result
+
+
+// console.log("I am after tackle()");
+// console.log(tackle());
+
+
+
+// Error handling //
+
+const error = new Promise((resolve, reject) => {
+    reject("error occured");
+})
+// error.catch((error) => console.log(error));
+
+async function handleError() {
+    try {
+        await error;
+    }
+    catch(error){
+        console.error(error);
+    }
+}
+
+handleError();
 
