700d76620c1bae1c9778c21e3cde4ede4a41a8e0
ad1tya0478 <aditya.50478@gmail.com>
2025-07-18 15:54:06 +0530

    Starting the movie app project

---
+++ html/study/tapascript/fetch.js
@@ -0,0 +1,5 @@
+// Fetch - fetch is a built-in JavaScript function used to make network requests, like getting or sending data to a server
+// Fetch API is the modern way in JavaScript to make HTTP requests - GET, POST, PUT, DELETE - without using old-school stuff like XMLHttpRequest
+
+fetch(URL, options)
+.then(response)
