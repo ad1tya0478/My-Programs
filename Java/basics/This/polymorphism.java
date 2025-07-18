9a2db1ac62fce2475bd130cedb1e7865facc26df
ad1tya0478 <aditya.50478@gmail.com>
2025-07-18 14:10:41 +0530

    Final Keyword

--- Java/basics/This/polymorphism.java
+++ Java/basics/This/polymorphism.java
@@ -5,16 +5,26 @@
 
 // }
 
-class a{
-    public void show(){
-        System.out.println("In A Show");
+// class a{
+//     public void show(){
+//         System.out.println("In A Show");
+//     }
+// }
+// class b extends a{
+//     public void show(){
+//         System.out.println("In B Show");
+//     }
+// }
+
+final class calc {
+    public final void show(){
+        System.out.println("in calc Show");
+    }
+    public void add(int a, int b){
+        System.out.println(a+b);
     }
 }
-class b extends a{
-    public void show(){
-        System.out.println("In B Show");
-    }
-}
+
 
 
 public class polymorphism {
@@ -28,17 +38,28 @@ public class polymorphism {
 // this polymorphism will only work with inheritance  
 
 
-        a obj = new a();
-        obj.show();
+        // a obj = new a();
+        // obj.show();
 
 // Computer lap = new Laptop();  // Laptop is a child class and computer is a parent class, We are creating a Laptop object 
         // but referencing it with a computer type this is called upcasting.
         // It allows runtime polymorphism, if laptop overRides a method from computer, the child's version runs. You can treat many subclasses as one type(parent)
         // Actual method depends on the object, not the reference type
 
-        obj = new b();
-        obj.show();
+        // obj = new b();
+        // obj.show();
 
+// Final keyword // variable/method/class
+// final in variable - value connot be changed once assigned, if you try to do it, it will give an error
+// final in method - connot be overriden by subclasses, it means if we use final in methods, it will not allow any subclass with same method to override it 
+// final class - cannot be extended(no subclass can inherit from it ), in this another class cannot inherit from the final class, it stops the inheritance
+
+        // final int num = 8;
+        // System.out.println(num);
+
+        calc obj = new calc();
+        obj.show();
+        obj.add(4,10);
 
     }
 }
